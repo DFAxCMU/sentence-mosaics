7b19e850daa7a4d1d38b32169dfa03a222b16da5
Edward Dryer <edryer@Edwards-MacBook-Pro.local>
2017-04-11 21:59:13 -0400

    add import image option

--- SentenceMosaics/android/app/src/main/java/com/sentencemosaics/MainApplication.java
+++ SentenceMosaics/android/app/src/main/java/com/sentencemosaics/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.imagepicker.ImagePickerPackage;
 import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
+            new ImagePickerPackage(),
             new RealmReactPackage()
       );
     }
--- SentenceMosaics/app/components/Home.js
+++ SentenceMosaics/app/components/Home.js
@@ -7,7 +7,10 @@ import {
   Alert,
   Button,
   TouchableHighlight,
-  ListView
+  ImagePickerIOS,
+  ListView,
+  AsyncStorage,
+
 } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import { connect } from 'react-redux'
@@ -23,10 +26,9 @@ let realm = new Realm({
 })
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
 ds.removeClippedSubviews = false
-var all_images = ds.cloneWithRows(
-  ['https://s-media-cache-ak0.pinimg.com/564x/21/c4/af/21c4af62d979e779f08730def389c7a4.jpg', 
+
+var images =  ['https://s-media-cache-ak0.pinimg.com/564x/21/c4/af/21c4af62d979e779f08730def389c7a4.jpg', 
   'https://pulsations.files.wordpress.com/2010/05/randomdog.jpg', 
-  'https://innerchildfun.com/wp-content/uploads/2013/02/lovepainting1.jpg', 
   'https://alpha.wallhaven.cc/wallpapers/thumb/small/th-439774.jpg',
   'https://s-media-cache-ak0.pinimg.com/736x/1c/2b/0e/1c2b0e1391f95210ce394d8f6ffc3349.jpg', 
   'https://az616578.vo.msecnd.net/files/responsive/embedded/any/desktop/2016/03/02/6359254205422825251433349260_pointe.jpg',
@@ -37,19 +39,32 @@ var all_images = ds.cloneWithRows(
   'https://static1.squarespace.com/static/53f25b20e4b0d6b2d8640379/t/553e2672e4b06614df78db39/1430136459547/', 
   'https://wesayyesprogram.com/wp-content/uploads/2015/07/man-walking-dog.jpg', 
   'https://ionehellobeautiful.files.wordpress.com/2015/02/mom-cooking-dinner.jpg?quality=70&strip=all&w=630&h=420'
-  ])
+  ];
 
-const Home = ({ onPhotoClick }) => (
+class ImageListView extends Component  {
+  constructor(props) {
+    super(props);
+    this.state = {all_images: ds.cloneWithRows(images)};
+    AsyncStorage.getItem("images").then((value) => {
+      if (value != null) {
+        images = JSON.parse(value);
+        this.setState({all_images: ds.cloneWithRows(images)});
+      }
+    }).done();
+  }
+
+  render() {
+    return (
   <View style={styles.container}>
     <ListView contentContainerStyle={styles.list}
-      dataSource={all_images}
+      dataSource={this.state.all_images}
       pageSize={9} // Needs to be a multiple of the number of
                    // cells per row or else they will be gaps
                    // when the rows are loaded
       renderRow={(rowData) =>
         <TouchableHighlight
           underlayColor='transparent'
-          onPress={onPhotoClick.bind(this, rowData)} >
+          onPress={this.props.onPhotoClick.bind(this, rowData)} >
         <Image
           style={styles.item}
           resizeMode='cover'
@@ -57,17 +72,24 @@ const Home = ({ onPhotoClick }) => (
         </TouchableHighlight>
       } />
     <Button title="Import Photos"
-            onPress={() => Alert.alert("Notice","Not yet Implemented!",[ {text : "Ok"}])}
+            onPress={() => ImagePickerIOS.openSelectDialog({}, imageUri => {
+              images.push(imageUri); 
+              this.setState({all_images: ds.cloneWithRows(images)});
+              var json_images = JSON.stringify(images); 
+              AsyncStorage.setItem("images", json_images);
+              }, error => {})}
             accessibilityLabel="Import New Photos" />
     <Button title="Take Photo"
             onPress={() => Alert.alert("Notice","Not yet Implemented!",[ {text : "Ok"}])}
             accessibilityLabel="Take a New Photo" />
+    </View>
+    );
+  }
+}
 
-
-
-
-
-  </View>
+var Home = ({ onPhotoClick }) => (
+    <ImageListView onPhotoClick={ onPhotoClick }
+     />
 )
 
 /* Container Component */
