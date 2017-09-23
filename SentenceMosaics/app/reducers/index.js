25ebcd8b09d7278330eb75d32823aa4cb21f9c11
Sashank Gogula <sashankg@gmail.com>
2017-09-22 21:39:35 -0400

    Removed unused routes reducer

--- SentenceMosaics/app/reducers/index.js
+++ SentenceMosaics/app/reducers/index.js
@@ -2,12 +2,10 @@
 
 import { combineReducers } from 'redux';
 
-import routes from './routes';
 import sentences from './sentences';
 import images from './images';
 
 const allReducers = combineReducers({
-  routes,
   sentences,
   images,
 });
