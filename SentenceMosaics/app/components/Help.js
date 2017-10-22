95340a9a1f3b4bde7a55e33971b1e634be6e3304
Edward Dryer <edryer@Edwards-MacBook-Pro.local>
2017-10-22 14:58:54 -0400

    add question component and add some FAQ

--- SentenceMosaics/app/components/Help.js
+++ SentenceMosaics/app/components/Help.js
@@ -7,11 +7,29 @@ import {
 } from 'react-native';
 import { styles } from '../styles';
 import { connect } from 'react-redux';
+import  Question  from './Question';
 
 
 const Help = ({ uri,sentences,remove_sentence }) => (
   <View style={styles.container}>
-    <Text>Help screen!</Text>
+
+  <View style={{margin: 20}} >
+    <Text style={{ fontSize: 48 }}>Frequently Asked Questions</Text>
+    <Question 
+        question="How do I delete images?"
+        answer="Double tap!"/>
+    <Question 
+        question="How do I delete words I have selected?"
+        answer="Double tap!"/>
+    <Question 
+        question="How do choose values for a word that I have added to my sentence?"
+        answer="Click the colored bubble that appears after you chose a part of speech"/>
+    <Question 
+        question="How do I play audio for a sentence that I saved?"
+        answer="Currently we do not support saving audio."/>
+
+  </View>
+
   </View>
 )
 
