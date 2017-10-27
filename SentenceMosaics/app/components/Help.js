569316dbd1acc126126a609f8ff673bf6ddb2ec6
jennifertchou <jennifertchou@gmail.com>
2017-10-26 22:32:13 -0400

    update FAQ

--- SentenceMosaics/app/components/Help.js
+++ SentenceMosaics/app/components/Help.js
@@ -3,6 +3,7 @@
 import React, { Component } from 'react';
 import {
   View,
+  ScrollView,
   Text,
 } from 'react-native';
 import { styles } from '../styles';
@@ -13,22 +14,27 @@ import  Question  from './Question';
 const Help = ({ uri,sentences,remove_sentence }) => (
   <View style={styles.container}>
 
-  <View style={{margin: 20}} >
+  <ScrollView style={{margin: 20}} >
     <Text style={{ fontSize: 48 }}>Frequently Asked Questions</Text>
     <Question 
-        question="How do I delete images?"
-        answer="Double tap!"/>
+        question="How do I delete an image?"
+        answer="Double tap an image to delete it."/>
     <Question 
-        question="How do I delete words I have selected?"
-        answer="Double tap!"/>
+        question="How do I create a sentence for an image?"
+        answer="Tap on an image for the option to create a new sentence for it."/>
+    <Question 
+        question="How do I add words to my sentence?"
+        answer="Click on a part of speech card to add an empty card to the end of the sentence."/>
     <Question 
         question="How do choose values for a word that I have added to my sentence?"
-        answer="Click the colored bubble that appears after you chose a part of speech"/>
+        answer="Click on the empty card that appears in the sentence."/>
     <Question 
-        question="How do I play audio for a sentence that I saved?"
-        answer="Currently we do not support saving audio."/>
-
-  </View>
+        question="How do I delete a word that I added to the sentence?"
+        answer="Double tap the word to delete it."/>
+    <Question 
+        question="How do I rearrange words in the sentence?"
+        answer="Long press a word in the sentence to move the word."/>
+  </ScrollView>
 
   </View>
 )
