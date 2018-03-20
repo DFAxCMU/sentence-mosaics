38430462b73a79797f891870ded1bdbdb438bf6a
jennifertchou <jennifertchou@gmail.com>
2018-03-20 14:25:53 -0400

    reword help screen

--- SentenceMosaics/app/components/Help.js
+++ SentenceMosaics/app/components/Help.js
@@ -23,11 +23,8 @@ const Help = () => (
         question="How do I create a sentence for an image?"
         answer="Tap on an image for the option to create a new sentence for it."/>
     <Question 
-        question="How do I add words to my sentence?"
-        answer="Click on a part of speech card to add an empty card to the end of the sentence."/>
-    <Question 
-        question="How do choose values for a word that I have added to my sentence?"
-        answer="Click on the empty card that appears in the sentence."/>
+        question="How do I add a word to my sentence?"
+        answer="Click on a card category to add an empty card to the end of the sentence. Then click the empty card to choose a word."/>
     <Question 
         question="How do I delete a word that I added to the sentence?"
         answer="Double tap the word to delete it."/>
